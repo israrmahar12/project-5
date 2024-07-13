e736d96810c58c0ebbe725202615ea0c9882e8a0
israrmahar12 <169028670+israrmahar12@users.noreply.github.com>
2024-07-13 00:53:08 -0700

    commit 5

---
+++ index.js
@@ -0,0 +1,47 @@
+const signup = async (e)=>{
+    e.preventDefault()
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    // console.log(email, password);
+
+ try {
+    const result = await firebase.auth().createUserWithEmailAndPassword(email, password);
+    console.log(result);
+    alert(`wellcom ${result.user.email}`)
+ } catch (error) {
+    console.log(error);
+    alert(error.message)
+ }
+
+}
+
+
+const Login = async (e)=>{
+    e.preventDefault()
+    const email = document.getElementById("login_email").value;
+    const password = document.getElementById("login_password").value;
+    // console.log(email, password);
+
+ try {
+    const result = await firebase.auth().signInWithEmailAndPassword(email, password);
+    console.log(result);
+    alert(`user is successfully login ${result.user.email}`)
+ } catch (error) {
+    console.log(error);
+    alert(error.message)
+ }
+
+}
+
+const logout = ()=>{
+   firebase.auth().signOut()
+   firebase.auth().onAuthStateChanged((user)=>{
+      if (user) {
+         console.log(user);
+         // ...
+      }else{
+         console.log(`user successfully signout`);
+         alert(`user successfully signout`)
+      }
+   });
+}
